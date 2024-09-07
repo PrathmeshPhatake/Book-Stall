5bf0980740773c0a0da0f597d310220e0294b4ab
Prathmesh Phatake <prathmeshPhatake4@gmail.com>
2024-09-08 01:16:14 +0530

    favicon

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,12 +4,13 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/AuthProvider.jsx";
-
+import BookIconComponent from './components/BookIconComponent.jsx';
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
       <div className="dark:bg-slate-900 dark:text-white">
         <App />
+        <BookIconComponent />
       </div>
     </AuthProvider>
   </BrowserRouter>
