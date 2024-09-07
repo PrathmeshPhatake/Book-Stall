7a5d490b6507a7a7bd81934a8efbf6902a1ebd4c
Prathmesh Phatake <prathmeshPhatake4@gmail.com>
2024-09-08 01:01:10 +0530

    faq

--- Frontend/src/about/about.jsx
+++ Frontend/src/about/about.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Footer from './footer'
 import Navbar from '../components/Navbar'
-
+import Faq from './faq'
 function about() {
   return (
    <>
    <Navbar/>
+   <Faq/>
    <Footer/>
    </>
   )
