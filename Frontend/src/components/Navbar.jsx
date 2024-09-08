b26d38a3ec9682afd964eb294838c56d73b4e2d3
Prathmesh Phatake <prathmeshPhatake4@gmail.com>
2024-09-09 00:23:28 +0530

    add Favicon

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -46,7 +46,7 @@ function Navbar() {
         <a href="/course">Course</a>
       </li>
       <li>
-        <a  href="/contact">Contact</a>
+        <a href="/contact">Contact</a>
       </li>
       <li>
         <a href="/about">About</a>
@@ -147,6 +147,29 @@ function Navbar() {
                 <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
               </svg>
             </label>
+
+            {/* for cart  */}
+            <div className="relative">
+              <button className="btn btn-ghost">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 3h2l.4 2m0 0L7 13a2 2 0 0 0 2 1h8a2 2 0 0 0 2-1l1.34-7H5.4m.34 0l-.34-2M7 13l-1.5 5.5m1.5-5.5L5 16m2-3h10M9 16h8a2 2 0 0 0 2-2M7 13l1.5 5.5m2-5.5L9 16M7 13l-1.5 5.5M17 11l1.5 5.5"
+                  />
+                </svg>
+              </button>
+              <span className="absolute top-0 right-1 bg-red-500 text-white text-xs rounded-lg px-2">
+                5
+              </span>
+            </div>
 
             {authUser ? (
               <Logout />
